ee6c04568e4fdf81985ab22eb2b99c64378a81e5
Faiz Kidwai <f.y.kidwai@gmail.com>
2020-10-18 18:58:27 +0530

    Validation added for customer

--- mssc-brewery/src/main/java/guru/springframework/msscbrewery/web/controller/CustomerController.java
+++ mssc-brewery/src/main/java/guru/springframework/msscbrewery/web/controller/CustomerController.java
@@ -1,12 +1,20 @@
 package guru.springframework.msscbrewery.web.controller;
 
+import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
+
+import javax.validation.ConstraintViolationException;
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -32,7 +40,7 @@ public class CustomerController {
     }
 
     @PostMapping
-    public ResponseEntity<Object> saveBeer(@RequestBody final CustomerDto customerDto) {
+    public ResponseEntity<Object> saveBeer(@Valid @RequestBody final CustomerDto customerDto) {
         final var customerDto2 = customerService.saveCustomer(customerDto);
         final var httpHeaders = new HttpHeaders();
         httpHeaders.add("Location", "/api/v1/customer/" + customerDto2.getId());
@@ -41,7 +49,7 @@ public class CustomerController {
 
     @PutMapping("/{customerId}")
     public ResponseEntity<Object> updateBeer(@PathVariable("customerId") final UUID customerId,
-        @RequestBody final CustomerDto customerDto) {
+        @Valid @RequestBody final CustomerDto customerDto) {
         customerService.updateCustomer(customerId, customerDto);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
@@ -52,4 +60,19 @@ public class CustomerController {
         customerService.deleteById(customerId);
     }
 
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<List<String>> validationExceptionHandler(final ConstraintViolationException exception) {
+        final var errors = exception.getConstraintViolations().stream()
+            .map(cv -> cv.getPropertyPath() + ":" + cv.getMessage()).collect(Collectors.toList());
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<List<String>> validationErrorHandler(final MethodArgumentNotValidException e) {
+        final var errors = e.getBindingResult().getAllErrors().stream().map(FieldError.class::cast)
+            .map(fieldError -> String.format("Bad Request %s : %s : Rejected value : ---> %s", fieldError.getField(),
+                fieldError.getDefaultMessage(), fieldError.getRejectedValue()))
+            .collect(Collectors.toList());
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }
 }
--- mssc-brewery/src/main/java/guru/springframework/msscbrewery/web/model/CustomerDto.java
+++ mssc-brewery/src/main/java/guru/springframework/msscbrewery/web/model/CustomerDto.java
@@ -2,6 +2,9 @@ package guru.springframework.msscbrewery.web.model;
 
 import java.util.UUID;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -13,5 +16,7 @@ import lombok.NoArgsConstructor;
 @Builder
 public class CustomerDto {
 	private UUID id;
+	@NotBlank
+	@Size(min = 3,max=100)
 	private String name;
 }
